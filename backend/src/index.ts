cd58313dd09b860a0c73731ccebd659be6fb3732
Roger Duong <z5428838@ad.unsw.edu.au>
2024-12-26 17:55:49 +1100

    set up 2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,34 @@
-import express, {Express, Request, Response} from 'express'
+import express, { Express, json, Request, Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import YAML from 'yaml';
+import sui from 'swagger-ui-express';
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Express = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("Hello World")
-})
+app.use(json());
+app.use(cors());
+app.use(morgan('dev'));
+
+// for producing the docs that define the API
+const file = fs.readFileSync(path.join(process.cwd(), 'swagger.yaml'), 'utf8');
+app.get('/', (req: Request, res: Response) => res.redirect('/docs'));
+app.use(
+  '/docs',
+  sui.serve,
+  sui.setup(YAML.parse(file), {
+    swaggerOptions: { docExpansion: false ? 'full' : 'list' },
+  })
+);
 
 app.listen(PORT, () => {
-    console.log(`Running on Port ${PORT}`)
+  console.log(`Running on Port ${PORT}`);
 });
